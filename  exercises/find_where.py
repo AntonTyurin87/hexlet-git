914b993f4e4e4e2c31d9740f3f3c2ad8472a2809
Антон Тюрин <AntonTyurin87@gmail.com>
2022-11-18 16:06:06 +0300

    add file find_where.py

---  exercises/find_where.py
+++  exercises/find_where.py
@@ -1,4 +1,6 @@
 
+#books = [0]
+
 books = [
     {'title': 'Book of Fooos', 'author': 'Foo', 'year': 1111},
     {'title': 'Cymbeline', 'author': 'Shakespeare', 'year': 1611},
@@ -8,21 +10,21 @@ books = [
     {'title': 'Happy Foo', 'author': 'FooBar', 'year': 4444},
 ]
 
+
+
 find = {'author': 'FooBar', 'year': 2222}
 
 def find_where(books, find):
-    flag = False
-    for n in books:
-        for k in find:
-            if find.get(k) == n.get(k):
-                flag = True
-            else:
-                flag = False
-        if flag:
-            return n
+    if len(find) == 0:
+         return books[0]
+    if set(find) - set(books[0]) == set():
+        for n in books:
+            if set(find.values()) - set(n.values()) == set():
+                return n
     return None
 
-print(find_where(books, find))
+print(books[2])
+print(find_where(books, books[2]))
 
 '''
 Реализуйте функцию find_where(), которая принимает на вход список книг и поисковый запрос и возвращает первую книгу, 
@@ -41,4 +43,32 @@ books = [
 
 find_where(books, {'author': 'Shakespeare', 'year': 1611})
 # {'title': 'Cymbeline', 'author': 'Shakespeare', 'year': 1611}
+
+def find_where(books, *args):
+    flag = False
+    for n in books:
+        for k in find:
+            if find.get(k) == n.get(k):
+                flag = True
+            else:
+                flag = False
+        if flag:
+            return n
+    return None
+
+def find_where(books, find):
+    if len(find) == 0:
+         return books[0]
+    if set(find) - set(books[0]) == set():
+        flag = False
+        for n in books:
+            for k in find:
+                if find.get(k) == n.get(k):
+                    flag = True
+                else:
+                    flag = False
+            if flag:
+                return n
+    return None
+
 '''

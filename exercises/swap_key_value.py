9a40571d4f7801579de50a96e25e2276232ecdf4
AntonTyurin87 <AntonTyurin87gmail.com>
2023-09-11 14:34:47 +0300

    post

--- exercises/swap_key_value.py
+++ exercises/swap_key_value.py
@@ -1,34 +1,12 @@
-from my_try import InMemoryKV
-import copy
+import requests
 
+'''
+response = requests.get('https://hexlet.io/lessons?page=2')
 
-map = InMemoryKV({'key': 10})
-map.set_('key2', 'value2')
+print(response)
+'''
 
+r = requests.get('https://api.github.com/events').json() 
+#r.text
 
-
-def swap_key_value(map):
-    dict_swap = map.to_dict()
-    a = []
-
-    for i in dict_swap:
-        a.append((dict_swap[i], i))
-        map.unset_(i)
-
-    for j in a:
-        map.set_(j[0], j[1])
-
-
-swap_key_value(map)
-
-print(map.get_('key'))
-print(map.get_(10))
-
-map = InMemoryKV({'foo': 'bar', 'bar': 'zoo'})
-
-print(map.to_dict())
-
-
-swap_key_value(map)
-
-print(map.to_dict()) #{'bar': 'foo', 'zoo': 'bar'}
+print(r)
